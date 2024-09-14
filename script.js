396d560a690ae0847a4ad6ef41203e516165af42
ShamsAlW <126289096+ShamsAlW@users.noreply.github.com>
2024-09-14 14:13:46 -0500

    OpenAI API Implementation

---
+++ script.js
@@ -0,0 +1,15 @@
+import { config } from 'dotenv'
+config()
+import { OpenAI } from 'openai'
+
+const openai = new OpenAI( { apiKey: process.env.API_KEY } );
+
+openai.chat.completions.create({ 
+    model: "gpt-4o",
+    messages: [
+        { role: "user", content: "Hello ChatGPT" }
+    ]
+}).then(res => {
+    console.log(res)
+    res.choices.forEach( out => console.log(out.message) );
+});
