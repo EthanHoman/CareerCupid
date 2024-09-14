1ea522c47f62092ce8c1f4733b3018db997c1d8e
ShamsAlW <salwaeli@icloud.com>
2024-09-14 14:43:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ import { config } from 'dotenv'
 config()
 import { OpenAI } from 'openai'
 
-const API_KEY = API_KEY=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
-const openai = new OpenAI( { apiKey: API_KEY } );
+const openai = new OpenAI( { apiKey: process.env.API_KEY } );
 
 openai.chat.completions.create({ 
     model: "gpt-4o",
