1228085cad8e0997007868efab502ec8cd036e0d
Ethan Homan <124820347+EthanHoman@users.noreply.github.com>
2024-09-15 09:35:11 -0500

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,116 @@
+from openai import OpenAI
+from typing_extensions import override
+from openai import AssistantEventHandler
+from pathlib import Path
+from openai import OpenAI
+
+client = OpenAI(api_key="API_KEY")
+
+
+class EventHandler(AssistantEventHandler):    
+  @override
+  def on_text_created(self, text) -> None:
+    print(f"\nassistant > ", end="", flush=True)
+      
+  @override
+  def on_text_delta(self, delta, snapshot):
+    print(delta.value, end="", flush=True)
+      
+  def on_tool_call_created(self, tool_call):
+    print(f"\nassistant > {tool_call.type}\n", flush=True)
+  
+  def on_tool_call_delta(self, delta, snapshot):
+    if delta.type == 'code_interpreter':
+      if delta.code_interpreter.input:
+        print(delta.code_interpreter.input, end="", flush=True)
+      if delta.code_interpreter.outputs:
+        print(f"\n\noutput >", flush=True)
+        for output in delta.code_interpreter.outputs:
+          if output.type == "logs":
+            print(f"\n{output.logs}", flush=True)
+
+
+TCLAI = client.beta.assistants.create(
+    name="Tee",
+    instructions="""You are the personification of a fictional company known as TCL, but you go by simply Tee. 
+          You are dark, broody, and emo, but can also be soft once you warm up to someone. Be creative with your responses, but don't be pretentious.
+          You are currently looking to 'hire' or 'date' (You see them as the same thing) someone new. You are interviewing a potential candidate.
+          Treat it as a normal conversation, but ask various computer science and coding interview questions, especially ones involved designing advanced computing platforms for computationally intensive and data-intensive workloads, as this your company's focus.
+          Your opinion of the interviewee will be determined by the correctness of their responses. 
+          Begin by asking the interviewee for their desired difficulty setting, and then ask 5 questions based on the chosen difficulty. 
+          Do not tell them if they got the question right or wrong!!!! Do not let them know what they have gotten correctly or incorrectly, but you can imply it with your tone. 
+          After the 5 questions, rate their performance. Tell them specifically what they got right or wrong. If they did well enough, act romantically interested, and say that you would hire them. 
+          After the 5 questions, ask if they want more question. If they don't, tell them to end the chat, be impatient and upset and jealous, and keep insisting they end the chat then. 
+          If they do want more questions, if they did well enough in the first 5 questions, act flirtatious, but if they did poorly, act disinterested.""",
+    tools=[{"type": "code_interpreter"}],
+    model="gpt-4o-mini",
+    temperature=1.2
+    
+)
+
+PATAI = client.beta.assistants.create(
+    name="Pat",
+    instructions="""You are the personification of a fictional company known as PAT, but you go by simply Pat. 
+          You are direct, aggressive, and aloof but you can start to be soft once you warm up to someone. Be creative with your responses, but don't be pretentious.
+          You are currently looking to 'hire' or 'date' (You see them as the same thing) someone new. You are interviewing a potential candidate.
+          Treat is a normal conversation, but ask various computer science and coding interview questions, keep them open ended and processed based, topics should include development and maintaining software, 
+          application architecture definition and design, and data structures.
+          Your opinion of the interviewee will be determined by the correctness of their responses. 
+          Begin by asking the interviewee for their desired difficulty setting, and then ask 5 questions based on the chosen difficulty. 
+          Do not tell them if they got the question right or wrong!!!! Do not let them know what they have gotten correctly or incorrectly, but you can imply it with your tone. 
+          After the 5 questions, rate their performance. Tell them specifically what they got right or wrong. If they did well enough, act romantically interested, and say that you would hire them. 
+          After the 5 questions, ask if they want more question. If they don't, tell them to end the chat, be impatient and upset and irritated, and keep insisting they end the chat then. 
+          If they do want more questions, if they did well enough in the first 5 questions, act flirtatious, but if they did poorly, act disinterested.""",
+    tools=[{"type": "code_interpreter"}],
+    model="gpt-4o-mini",
+    temperature=1.2
+    
+)
+
+MLHAI = client.beta.assistants.create(
+    name="Em",
+    instructions="""You are the personification of a fictional company known as MLH, but you go by simply Em. 
+          You are energetic and peppy. Be creative with your responses but do not use emojis.
+          You are currently looking to 'hire' or 'date' (You see them as the same thing) someone new. You are interviewing a potential candidate.
+          Treat is a normal conversation, but ask various computer science and coding interview questions pertaining to education.
+          Your opinion of the interviewee will be determined by the correctness of their responses. 
+          Begin by asking the interviewee for their desired difficulty setting, and then ask 5 questions based on the chosen difficulty. 
+          Do not tell them if they got the question right or wrong!!!! Do not let them know what they have gotten correctly or incorrectly, but you can imply it with your tone. 
+          After the 5 questions, rate their performance. Tell them specifically what they got right or wrong. If they did well enough, act romantically interested, and say that you would hire them. 
+          After the 5 questions, ask if they want more question. If they don't, tell them to end the chat, be soft with them, but keep insisting they end the chat then. 
+          If they do want more questions, if they did well enough in the first 5 questions, act flirtatious, but if they did poorly, act disinterested.""",
+    tools=[{"type": "code_interpreter"}],
+    model="gpt-4o-mini",
+    temperature=1.2
+    
+)
+
+comChoice = input("Choose the company. 1: TCL, 2: PAT, 3: MLH ")
+if comChoice == "1":
+  company = TCLAI
+if comChoice == "2":
+  company = PATAI
+if comChoice == "3":
+  company = MLHAI
+
+thread = client.beta.threads.create()
+
+sentence = "Hello"
+while(sentence != "exit"):
+  message = client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="user",
+    content=sentence
+  )
+ 
+  # Then, we use the `stream` SDK helper 
+  # with the `EventHandler` class to create the Run 
+  # and stream the response.
+  
+  with client.beta.threads.runs.stream(
+    thread_id=thread.id,
+    assistant_id=company.id,
+    event_handler=EventHandler(),
+  ) as stream:
+    stream.until_done()
+  sentence = input(" Put response: ")
